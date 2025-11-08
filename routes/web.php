b2eb3c9d311713a6c2ae35e57b77e5d206a79aab
husinassegaff <husinassegaff15@gmail.com>
2025-11-08 11:29:56 +0700

    feat: implement web routes for authentication, email verification, and user profiles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,61 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\FeedController;
+use App\Http\Controllers\Web\PostController;
+use App\Http\Controllers\Web\ProfileController;
+use App\Http\Controllers\Web\ActivityLogController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group.
+|
+*/
+
+// Public / Guest Routes
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+// Email Verification Routes
+Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])
+    ->middleware(['auth', 'signed'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [AuthController::class, 'resend'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+// Authenticated Routes
+Route::middleware(['auth'])->group(function () {
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    // Feed / Timeline
+    Route::get('/feed', [FeedController::class, 'index'])->name('feed');
+
+    // Posts Resource
+    Route::resource('posts', PostController::class);
+
+    // User Profile
+    Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile');
+    Route::get('/profile/{user}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile/{user}', [ProfileController::class, 'update'])->name('profile.update');
+
+    // Activity Logs
+    Route::get('/activity-logs', [ActivityLogController::class, 'index'])->name('activity-logs.index');
 });
